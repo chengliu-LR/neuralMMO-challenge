fadf9a6d3ec40431c374f8e38f70fcd12f6bca6c
Cheng <c.liu-10@tudelft.nl>
2022-06-10 04:02:56 +0200

    Changes for submission-protoss-forage-bar-0610-pvp

--- my-submission/scripted/baselines.py
+++ my-submission/scripted/baselines.py
@@ -33,8 +33,8 @@ class Scripted(nmmo.Agent):
     def forage_criterion(self) -> bool:
         '''Return true if low on food or water'''
         # this parameter can be tuned
-        food_min_level = 0.86 * self.food_max
-        water_min_level = 0.86 * self.water_max
+        food_min_level = 0.8 * self.food_max
+        water_min_level = 0.8 * self.water_max
         return self.food <= food_min_level or self.water <= water_min_level
 
     def forage(self):
@@ -257,8 +257,8 @@ class Protoss(Scripted):
         self.adaptive_control_and_targeting()
 
         #self.style = nmmo.action.Range
-        self.select_combat_style()
-        #self.protoss_combat()
+        #self.select_combat_style()
+        self.protoss_combat()
         self.attack()
 
         return self.actions
